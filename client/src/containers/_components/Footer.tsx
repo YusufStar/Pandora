af87736fc2cf80f5b57e3050bb56271b72af54ed
Yusuf Yıldız <yyilidz518@gmail.com>
2024-04-24 20:49:40 +0300

    started footer coding

--- client/src/containers/_components/Footer.tsx
+++ client/src/containers/_components/Footer.tsx
@@ -1,10 +1,21 @@
 "use client";
+import Payments from "@/components/icons/Payments";
 import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-black">
-      <div className="container mx-auto mb-8 px-4 py-2 w-full min-h-[300px]"></div>
+    <footer className="bg-black dark">
+      <div className="container flex flex-col justify-between mx-auto mb-8 px-12 py-2 w-full min-h-[300px]">
+        <div className="h-fit"></div>
+        <div className="flex w-full items-center justify-between">
+          <h3>
+            <span className="text-[14px] font-medium text-white/50">
+              © 2023 MAJOLİKA
+            </span>
+          </h3>
+          <Payments />
+        </div>
+      </div>
     </footer>
   );
 };
