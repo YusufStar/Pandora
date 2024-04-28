5c65aff21a08fec287887cc70e8142a7dc9c7656
Yusuf Yıldız <yyilidz518@gmail.com>
2024-04-28 17:40:36 +0300

    Update route.ts

--- client/src/app/api/auth/[...nextauth]/route.ts
+++ client/src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,5 @@
 import NextAuth from "next-auth";
 import authOptions from "@/lib/auth-options";
 
-
-// Export a default function for the NextAuth route
 const route = NextAuth(authOptions)
 export {route as GET, route as PATH}
