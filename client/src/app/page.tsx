225d616804867a819a911ace51c17a2c36400dce
Yusuf Yıldız <yyilidz518@gmail.com>
2024-04-28 17:34:24 +0300

    x

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,13 +1,8 @@
 import React from "react";
-import {getServerSession} from "next-auth";
-import {authOptions} from "@/app/api/auth/[...nextauth]/route";
 
-const Page = async () => {
-  const session = await getServerSession(authOptions)
-
+const Page = () => {
   return <main>
-<h2>Server Session</h2>
-    <pre>{JSON.stringify(session)}</pre>
+      page
   </main>;
 };
 
