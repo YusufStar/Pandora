912896476e706570930472f42e940d869e1c7ff6
yusuf <yyilidz518@gmail.com>
2024-07-18 12:33:03 +0300

    x

---
+++ server/Yurtici.js
@@ -0,0 +1,108 @@
+var Yurtici = {},
+  request = require("request");
+
+/**
+ * @private
+ */
+Yurtici._queryFormatter = function (query) {
+  var postingRefIds;
+
+  if (typeof query === "string") {
+    postingRefIds = query.split();
+  } else if (typeof query === "number") {
+    postingRefIds = String(query).split();
+  } else {
+    query = query.map(function (item) {
+      return String(item);
+    });
+
+    return query;
+  }
+
+  return postingRefIds;
+};
+
+/**
+ * @private
+ */
+Yurtici._prepareQuery = function (query) {
+  var postingRefIds = Yurtici._queryFormatter(query),
+    innerQuery = {
+      postingRefIds: postingRefIds,
+      ticketId: "8a81a3194f830a88014f88343d881642",
+      SERVICE_NAME: "getLastPostings",
+    };
+
+  return {
+    NO_ENC: true,
+    INPUT_BLOCK: JSON.stringify(innerQuery),
+  };
+};
+
+Yurtici.replace_tr = function (input) {
+  input = input.replace("@s", "ş");
+  input = input.replace("@i", "ı");
+  input = input.replace("@g", "ğ");
+  input = input.replace("@u", "ü");
+  input = input.replace("@c", "ç");
+  input = input.replace("@o", "ö");
+  return input;
+};
+
+Yurtici.query = function (query, callback) {
+  request(
+    {
+      uri: "http://mapp.yurticikargo.com/ListenerServlet",
+      method: "POST",
+      form: Yurtici._prepareQuery(query),
+      json: false,
+    },
+    function (error, response, body) {
+      if (error) return callback(error);
+
+      var responseObj = JSON.parse(Yurtici.replace_tr(body));
+
+      if (responseObj.message) return callback(new Error(responseObj.message));
+
+      return callback(null, responseObj.result);
+    }
+  );
+};
+
+/**
+ * @private
+ */
+Yurtici._prepareMovementsQuery = function (singleId) {
+  var innerQuery = {
+    postingRefId: singleId,
+    ticketId: "8a81a3194f830a88014f88343d881642",
+    SERVICE_NAME: "getCargoMovements",
+  };
+
+  return {
+    NO_ENC: true,
+    INPUT_BLOCK: JSON.stringify(innerQuery),
+  };
+};
+
+Yurtici.getMovements = function (query, callback) {
+  request(
+    {
+      uri: "http://mapp.yurticikargo.com/ListenerServlet",
+      method: "POST",
+      form: Yurtici._prepareMovementsQuery(query),
+      json: false,
+    },
+    function (error, response, body) {
+      if (error) return callback(error);
+
+      var responseObj = JSON.parse(Yurtici.replace_tr(body));
+
+      if (responseObj.message) return callback(new Error(responseObj.message));
+
+      return callback(null, responseObj.result);
+    }
+  );
+};
+
+module.exports = Yurtici;
--- server/app.js
+++ server/app.js
@@ -1,23 +1,27 @@
 //* Import dependencie
 const express = require("express");
 const http = require("http");
-const bodyParser = require("body-parser")
-const Iyzipay = require("iyzipay")
-const cors = require("cors")
+const bodyParser = require("body-parser");
+const Iyzipay = require("iyzipay");
+const cors = require("cors");
+const yurtici = require("./Yurtici");
 
 //* Intialize env veriables
 const { config } = require("dotenv");
+const Yurtici = require("./Yurtici");
 config();
 
 //* Setup the express server
 const app = express();
 
-app.use(cors({
-  allowedHeaders: ["*"],
-  origin: "*",
-  methods: ["POST", "GET", "PUT"]
-}))
-app.use(bodyParser.json())
+app.use(
+  cors({
+    allowedHeaders: ["*"],
+    origin: "*",
+    methods: ["POST", "GET", "PUT"],
+  })
+);
+app.use(bodyParser.json());
 
 const server = http.createServer(app);
 const port = process.env.PORT || 3000;
@@ -28,19 +32,29 @@ app.use(express.json({ limit: "1mb" }));
 const iyzipay = new Iyzipay({
   apiKey: process.env.IYZICO_API_KEY,
   secretKey: process.env.IYZICO_SECRET_KEY,
-  uri: process.env.IYZICO_BASE_URL
-})
+  uri: process.env.IYZICO_BASE_URL,
+});
 
 app.post("/api/payment", (req, res) => {
-  const {price, paidPrice, currency, basketId, paymentCard, buyer, shippingAddress, billingAddress, basketItems} = req.body
+  const {
+    price,
+    paidPrice,
+    currency,
+    basketId,
+    paymentCard,
+    buyer,
+    shippingAddress,
+    billingAddress,
+    basketItems,
+  } = req.body;
 
   const request = {
     locale: "TR",
-    conversationId: '123456789',
+    conversationId: "123456789",
     price: price,
     paidPrice: paidPrice,
     currency: currency,
-    installment: '1',
+    installment: "1",
     basketId: basketId,
     paymentChannel: "WEB",
     paymentGroup: "PRODUCT",
@@ -48,18 +62,18 @@ app.post("/api/payment", (req, res) => {
     buyer: buyer,
     shippingAddress: shippingAddress,
     billingAddress: billingAddress,
-    basketItems: basketItems
-};
+    basketItems: basketItems,
+  };
 
-iyzipay.payment.create(request, (err, result) => {
-  if (err) {
-    return res.status(500).json(err)
-  }
-  res.status(200).json(result)
-})
-})
+  iyzipay.payment.create(request, (err, result) => {
+    if (err) {
+      return res.status(500).json(err);
+    }
+    res.status(200).json(result);
+  });
+});
 
 //* Start the server
 server.listen(port, () => {
-  console.log(`Server started, PORT -> ${port}`)
+  console.log(`Server started, PORT -> ${port}`);
 });
